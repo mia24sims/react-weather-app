833e870b153234860023bc61ba711ce58e548c85
mia24sims <167013397+mia24sims@users.noreply.github.com>
2025-09-17 14:16:35 -0500

    Added rel="nonreferrer" to links

--- src/App.js
+++ src/App.js
@@ -6,12 +6,21 @@ function App() {
     <div className="App">
       Weather App
       <footer>
-        This project was coded by{" "} 
-        <a href="https://github.com/mia24sims?tab=projects" target="_blank">Tamia Sims</a>
-                {" "}  and is{" "}
+        This project was coded by{" "}
+        <a
+          href="https://github.com/mia24sims?tab=projects"
+          target="_blank"
+          rel="nonreferrer"
+        >
+          Tamia Sims
+        </a>{" "}
+        and is{" "}
       </footer>
-      <a href="https://github.com/mia24sims/react-weather-app" target="_blank">
-
+      <a
+        href="https://github.com/mia24sims/react-weather-app"
+        target="_blank"
+        rel="nonreferrer"
+      >
         open-sourced on GitHub
       </a>
     </div>
