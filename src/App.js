9a75bd4cf4b01e7c295fdf04678ff0bb7df93e4d
mia24sims <167013397+mia24sims@users.noreply.github.com>
2025-09-17 18:22:02 -0500

    re-re did previous steps

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/mia24sims?tab=repositories"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          Learn React
-        </a>
-      </header>
+          Tamia Sims{" "}
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/mia24sims/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+      </footer>
     </div>
   );
 }
-
-export default App;
