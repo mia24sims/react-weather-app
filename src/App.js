43c43ab9d5bead2ac62cc8a257385a267796c8d5
mia24sims <167013397+mia24sims@users.noreply.github.com>
2025-09-17 14:13:42 -0500

    removed logo import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-  Weather App
+      Weather App
+      <footer>
+        This project was coded by{" "} 
+        <a href="https://github.com/mia24sims?tab=projects" target="_blank">Tamia Sims</a>
+                {" "}  and is{" "}
+      </footer>
+      <a href="https://github.com/mia24sims/react-weather-app" target="_blank">
+
+        open-sourced on GitHub
+      </a>
     </div>
   );
 }
