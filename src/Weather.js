7cc23b9115e18fa66668a0588d3d88ad4dcf1e98
mia24sims <167013397+mia24sims@users.noreply.github.com>
2025-09-18 16:21:42 -0500

    Added date

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import FormattedDate from "./FormattedDate";
 import "./Weather.css";
 
 export default function Weather(props) {
@@ -10,7 +11,7 @@ export default function Weather(props) {
       ready: true,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
-      date: "Wednesday 07:00",
+      date: new Date(response.data.dt*1000),
       description: response.data.weather[0].description,
       iconUrl:
         "https://www.gstatic.com/weather/conditions2023/2023.2/svg/cloudy_light.svg",
@@ -46,7 +47,9 @@ export default function Weather(props) {
 
         <h1>{weatherData.city}</h1>
         <ul>
-          <li>{weatherData.date}</li>
+          <li>
+            <FormattedDate date={weatherData.date}/>
+          </li>
           <li className="text-capitalize">{weatherData.description}</li>
         </ul>
         <div className="row">
