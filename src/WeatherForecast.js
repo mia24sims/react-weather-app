c7bd982b82986f4945a5fa6a6574a9979e130c88
mia24sims <167013397+mia24sims@users.noreply.github.com>
2025-10-03 09:42:03 -0500

    fixed warning

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useLayoutEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -31,6 +31,8 @@ useEffect(() => {
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
